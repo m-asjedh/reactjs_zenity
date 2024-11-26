e6e0fe9fadf7897ac706b482b6a537325750c850
asjedh-dilimatrix <Mohamed.Asjedh@dilimatrix.com>
2024-11-26 07:36:07 +0530

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Button = ({ title, id, leftIcon, containerClass }) => {
+  return (
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}
+    >
+      {leftIcon}
+      <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
+        <div>{title}</div>
+      </span>
+    </button>
+  );
+};
+
+export default Button;
